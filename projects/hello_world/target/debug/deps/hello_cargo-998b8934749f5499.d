33865e83d11fd6ad599431684c571a09f8a014e1
arnabnandikgp <arnabnandi2002@gmail.com>
2024-04-10 02:35:12 +0530

    init
    Signed-off-by: arnabnandikgp <arnabnandi2002@gmail.com>

---
+++ projects/hello_world/target/debug/deps/hello_cargo-998b8934749f5499.d
@@ -0,0 +1,5 @@
+/home/arnab/learn_rust/projects/hello_world/target/debug/deps/hello_cargo-998b8934749f5499: src/main.rs
+
+/home/arnab/learn_rust/projects/hello_world/target/debug/deps/hello_cargo-998b8934749f5499.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_world/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/arnab/learn_rust/projects/hello_world/target/debug/hello_cargo: /home/arnab/learn_rust/projects/hello_world/src/main.rs
